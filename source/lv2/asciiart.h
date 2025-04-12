9bd9c96fbc00da69ffad02e1f2d372deed9a7513
keta <153708582+killaketa@users.noreply.github.com>
2025-04-12 02:50:05 -0400

    Update asciiart.h

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -1,10 +1,10 @@
 static char* asciiart = "\n"
-
-	"        xxxxxx   xxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxx  xxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxx        xxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxx\n"
-	"       xxxxxxx xxxxxxxx xxxxxx  xxxxxx xxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx     xxxxxx  xxxxxxx        xxxxxx            xxxxxxx   xxxxxx  xxxxxxxxxxxxxx xxxxxx xxxxxx       \n"
-	"     xxxxxxxxxxxxxxxxx xxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxx  xxxxxxXxxxxxxxxxxx xxxxxxx xxxxxx          xxxxxxxxxxxx     xxxxxx    xxxxxx  xxxxxx xxxxxxxxxxxxxx xxxxxxxxxx    \n"
-	"    XXX XXXXXXXXXXXxXXXXXXXx xXXXXX XXXXXx  XXXXXX XXXXXX  XXXXXXxXXXXXX      XXXXXX  XXXXXX  XXXX  XXXXXXX XXXXXXX    XXXXXX    XXXXXX  XXXXXx XXXXXx XXXXXX XXXXXXX         \n"
-	"   XXX  XXXX XXXXXX  XXXXXXXXXXXXX XXXXXXXXXXXXX  XXXXXXXXXXXXX  XXXXXXXXXXX XXXXXXXXXXXXXX  XXXXX  XXXXXXXXXXXXX     XXXXXX     XXXXXXXXXXXXX XXXXXX  XXXXXX XXXXXXXXXXX     \n";
+	"    :::::   :::::: ::::::::::: :::::::::::: ::::::::::::;:::::::::;::::::::::::       ::::::::::::::::::::::: :::::::::::: ::::::::::::::::::::::\n"
+	"   ++++++  ++++++ +++++  +++++x++++  +++++ +++++  +++++ +++++     +++++  +++++       +++++  +++++   ++++++   +++++  +++++ +++++  +++++x+++++     \n"
+	"  XXxXXXXXXxXXXX XXXXX  XXXXX xXXXX XXXXXXXXXXX  XXXXx XXXXXXXX  XXXXX  XXXXx       XXXXXXXXXX      XXXXX   XxXXX  XXXXX XXXXx  XXXXx XXXXXXXX   \n"
+	"  $$$$$$$$$$$$$ $$$$$  $$$$$ $$$$$  $$$$$&$$$$$ $$$$$$$$$$$&&&  $$$$$  $$$$$          &&&&$$$$$    $$$$$    $$$$$  $$$$$$$$$$$$$$$$$ $$$$$&&&    \n"
+	" && &&&&&&&&&&&&&&&&  &&&&& &&&&&  &&&&& &&&&& &&&&&&&&&&&&    &&&&&& &&&&&&       &&&&& &&&&&    &&&&&    &&&&& &&&&&& &&&&&&&&&&  &&&&&        \n"
+	"&&& &&&& &&&&& &&&&&&&&&&&&&&&&&&&&&&&& &&&&&&&&&&&& &&&&&&&&& &&&&&&&&&&&& &&&&& &&&&&&&&&&&&   &&&&&&   &&&&&&&&&&&& &&&&& &&&&&  &&&&&&&&&    \n"
                                                                                                                                                                                 
 static char* asciitail =
 	"              Free60.org XeLL RELOADED - Xenon Linux Loader\n              Console Serviced + Modded by Modded.Store\n";

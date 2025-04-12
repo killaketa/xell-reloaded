00b3af0b7fab17417012718e23b8e7508554cb61
keta <153708582+killaketa@users.noreply.github.com>
2025-04-12 02:43:21 -0400

    Update asciiart.h

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -1,8 +1,10 @@
 static char* asciiart = "\n"
-	"• ▌ ▄ ·.       ·▄▄▄▄  ·▄▄▄▄  ▄▄▄ .·▄▄▄▄     .▄▄ · ▄▄▄▄▄      ▄▄▄  ▄▄▄ .\n"
-	"·██ ▐███▪▪     ██▪ ██ ██▪ ██ ▀▄.▀·██▪ ██    ▐█ ▀. •██  ▪     ▀▄ █·▀▄.▀·\n"
-	"▐█ ▌▐▌▐█· ▄█▀▄ ▐█· ▐█▌▐█· ▐█▌▐▀▀▪▄▐█· ▐█▌   ▄▀▀▀█▄ ▐█.▪ ▄█▀▄ ▐▀▀▄ ▐▀▀▪▄\n"
-	"██ ██▌▐█▌▐█▌.▐▌██. ██ ██. ██ ▐█▄▄▌██. ██    ▐█▄▪▐█ ▐█▌·▐█▌.▐▌▐█•█▌▐█▄▄▌\n"
-	"▀▀  █▪▀▀▀ ▀█▄▀▪▀▀▀▀▀• ▀▀▀▀▀•  ▀▀▀ ▀▀▀▀▀•  ▀  ▀▀▀▀  ▀▀▀  ▀█▄▀▪.▀  ▀ ▀▀▀ \n";
+
+	"        xxxxxx   xxxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxx  xxxxxxxxxxxxx  xxxxxxxxxxxxxxxxxxxxxxxxxx        xxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+	"       xxxxxxx xxxxxxxx xxxxxx  xxxxxx xxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxxx     xxxxxx  xxxxxxx        xxxxxx            xxxxxxx   xxxxxx  xxxxxxxxxxxxxx xxxxxx xxxxxx       \n"
+	"     xxxxxxxxxxxxxxxxx xxxxxx xxxxxxxxxxxxxx xxxxxxxxxxxxx  xxxxxxXxxxxxxxxxxx xxxxxxx xxxxxx          xxxxxxxxxxxx     xxxxxx    xxxxxx  xxxxxx xxxxxxxxxxxxxx xxxxxxxxxx    \n"
+	"    XXX XXXXXXXXXXXxXXXXXXXx xXXXXX XXXXXx  XXXXXX XXXXXX  XXXXXXxXXXXXX      XXXXXX  XXXXXX  XXXX  XXXXXXX XXXXXXX    XXXXXX    XXXXXX  XXXXXx XXXXXx XXXXXX XXXXXXX         \n"
+	"   XXX  XXXX XXXXXX  XXXXXXXXXXXXX XXXXXXXXXXXXX  XXXXXXXXXXXXX  XXXXXXXXXXX XXXXXXXXXXXXXX  XXXXX  XXXXXXXXXXXXX     XXXXXX     XXXXXXXXXXXXX XXXXXX  XXXXXX XXXXXXXXXXX     \n";
+                                                                                                                                                                                
 static char* asciitail =
-	"              Free60.org XeLL RELOADED - Xenon Linux Loader\n Console Serviced + Modded by Modded.Store\n";
+	"              Free60.org XeLL RELOADED - Xenon Linux Loader\n              Console Serviced + Modded by Modded.Store\n";

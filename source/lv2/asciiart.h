3a73e3e343f3d2d90d286d1f53d93f08f819fc58
keta <153708582+killaketa@users.noreply.github.com>
2025-04-12 03:18:24 -0400

    Update asciiart.h

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -4,7 +4,7 @@ static char* asciiart = "\n"
 	"  ++;;;;;++;;;++ ++;++  +++++ +++++  +++++ +++++ +++++  ++;++xx   +++++  +++++        +++++xxxx      +++++    +++++  +++++ ++;++  +++++ ++;;++xx   \n"
 	"  xx++++++++++x xx++x  x+++x xx++x  x+++x x+++x  x++xx x+++xxxx  xx++x  xx++x         xxxxxx++xx    xx++x    xx++x  xx++x x++++++++xx  xx+++xxxx   \n"
 	" XX xxxx xxxxX XxxxX  XxxxX Xxxxx  XxxxX Xxxxx  XxxxX Xxxxx     Xxxxx  XxxxX       XXXXX XxxxXX    XxxXX    Xxxxx  XxxxX XXxxxxxxxxX  Xxxxx        \n"
-	"$$  $$$  $$$$ $$XX$$$$$$$$ $$$XX$$$$$   $$$XX$$$$$   $$$XX$$$$ $$XXX$$$$$$    $$$$ $$$$$$$$$$$$   $$$$$    $$$XXX$$$$$$ $$$$$  $$$$$ $$XX$$$$$     \n"
+	"$$  $$$  $$$$ $$XX$$$$$$$$ $$$XX$$$$$   $$$XX$$$$$   $$$XX$$$$ $$XXX$$$$$$    $$$$ $$$$$$$$$$$$   $$$$$    $$$XXX$$$$$$ $$$$$  $$$$$ $$XX$$$$$     \n";
                                                                                                                                                                                 
 static char* asciitail =
 	"              Free60.org XeLL RELOADED - Xenon Linux Loader\n              Console Serviced + Modded by Modded.Store\n";

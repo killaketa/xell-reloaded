3700a547c6ff9a8e9a294c5d798c731b98ce12fd
keta <153708582+killaketa@users.noreply.github.com>
2025-04-12 03:08:33 -0400

    Update asciiart.h

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -1,10 +1,10 @@
 static char* asciiart = "\n"
-	"    :::::   :::::: :::::::::::: ::::::::::    :::::::::   ;::::::::: ::::::::::          :::::::::::  ::::::::::: ;::::::::::: ::::::::::::  :::::::::\n"
-	"   ;;;;;;  ;;;;;; ;;;;;  ;;;;; ;;;;;; ;;;;;  ;;;;; ;;;;;  ;;;;;     ;;;;;; ;;;;;        ;;;;;  ;;;;;    ;;;;;    ;;;;;  ;;;;; ;;;;;  ;;;;; ;;;;;;     \n"
-	"  ++;;;;+++;;;++ +++++  +++++ ++++++ +++++  +++++ +++++  ++;;++x+  ++++++ ++++++       ++++++xxx+      +++++     +++++ +++++  ++;++ ++++++ ++;;+xx+   \n"
-	"  xx++++++++++x  x++xx xx++x  x++xx  x++xx x+++x  x++xx x++++xxxx  x+++x  x++xx         xxxxx+++xx    xx++x     x++xx  x++xx x+++++x++xxx x+++xxxxx   \n"
-	" XX xxxx xxxxXX XxxxX XXxxxX Xxxxx  XxxxX Xxxxx  XxxxX XxxxxX     Xxxxx  XxxxX        XXXXX  xxxXX    XxxXX    Xxxxx  XxxxX Xxxxx xxxxX  Xxxxx        \n"
-	"$$$ $$$  $$$$$ $$$XX$$$$$$$ $$XXX$$$$$   $$$XX$$$$$    $$XX$$$$$ $$$XX$$$$$     $$$  $$$$$$$$$$$$    $$$$$    $$$XX$$$$$$$ $$$$$  $$$$$  $$XX$$$$$    \n";
+	"    :::::   :::::: :::::::::::: ::::::::::    :::::::::   ;::::::::: ::::::::::          :::::::::::  :::::::::::  :::::::::::: :::::::::::   :::::::::\n"
+	"   ;;;;;;  ;;;;;; ;;;;;  ;;;;; ;;;;;; ;;;;;  ;;;;; ;;;;;  ;;;;;     ;;;;;; ;;;;;        ;;;;;  ;;;;;    ;;;;;     ;;;;;  ;;;;; ;;;;;  ;;;;;  ;;;;;     \n"
+	"  ++;;;;+++;;;++ +++++  +++++ ++++++ +++++  +++++ +++++  ++;;++x+  ++++++ ++++++       ++++++xxx+      +++++     +++++  +++++ ++;++ ++++++  ++;;+xx+   \n"
+	"  xx++++++++++x  x++xx xx++x  x++xx  x++xx x+++x  x++xx x++++xxxx  x+++x  x++xx         xxxxx+++xx    xx++x     x++xx  x++xx x+++++x++xxx  x+++xxxx    \n"
+	" XX xxxx xxxxXX XxxxX XXxxxX Xxxxx  XxxxX Xxxxx  XxxxX XxxxxX     Xxxxx  XxxxX        XXXXX  xxxXX    XxxXX    Xxxxx  XxxxX Xxxxx xxxx    Xxxxx        \n"
+	"$$$ $$$  $$$$$ $$$XX$$$$$$$ $$XXX$$$$$   $$$XX$$$$$    $$XX$$$$$ $$$XX$$$$$     $$$  $$$$$$$$$$$$    $$$$$    $$$XX$$$$$$$ $$$$$  $$$$$  $$XX$$$$$     \n";
                                                                                                                                                                                 
 static char* asciitail =
 	"              Free60.org XeLL RELOADED - Xenon Linux Loader\n              Console Serviced + Modded by Modded.Store\n";
